79700bb577466f393f7d2e839909c75905ad1c51
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-21 06:34:37 +0700

    js lab 67

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// write code here
+var prompt = require('prompt-sync')();
+function Calculator(a, b) {
+  this.read = function() {
+    this.a = +prompt("a:", 0);
+    this.b = +prompt("b:", 0);
+  };
+  this.sum = function() {
+    return this.a + this.b;
+  };
+  this.mul = function() {
+    return this.a * this.b;
+  };
+};
+
+let cal = new Calculator();
+cal.read()
+console.log(cal.sum());
+console.log(cal.mul());
+
